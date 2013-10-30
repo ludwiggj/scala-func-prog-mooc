46a2b92f814e8e7d47a1cfed8b1e6a09d1802db6
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2013-10-30 13:03:26 +0000

    [Graeme] Wk 7 stream fun

---
+++ lectures/src/main/week7/streams/primes.scala
@@ -0,0 +1,53 @@
+package main.week7.streams
+
+object primes {
+  def isPrime(n: Int): Boolean = {
+    (2 until n) forall (x => n % x != 0)
+  }
+
+  def nthPrime(from: Int, to: Int, n: Int): Int = {
+    if (from >= to) {
+      throw new Error("no prime")
+    }
+    else if (isPrime(from)) {
+      if (n == 1) from else nthPrime(from + 1, to, n - 1)
+    }
+    else nthPrime(from + 1, to, n)
+  }
+
+  def main(args: Array[String]) = {
+    println("Calculate primes using recursion....")
+
+    println("  First prime = " + nthPrime(1000, 10000, 2))
+    println("  Sixth prime = " + nthPrime(1000, 10000, 6))
+
+    val primeList: List[Int] = (1000 to 1050).toList filter isPrime
+
+    println("\nCalculate primes with a List[Int]....")
+    println("  primes [" + primeList + "]\n")
+
+    println("  First prime = " + primeList(0))
+    println("  primes [" + primeList + "]\n")
+
+    println("  Second prime = " + primeList(1))
+    println("  primes [" + primeList + "]\n")
+
+    println("  Sixth prime = " + primeList(5))
+    println("  primes [" + primeList + "]\n")
+
+
+    val primeStream: Stream[Int] = (1000 to 1050).toStream filter isPrime
+
+    println("Calculate primes with a Stream[Int]....")
+    println("  primes [" + primeStream + "]\n")
+
+    println("  First prime = " + primeStream(0))
+    println("  primes [" + primeStream + "]\n")
+
+    println("  Second prime = " + primeStream(1))
+    println("  primes [" + primeStream + "]\n")
+
+    println("  Sixth prime = " + primeStream(5))
+    println("  primes [" + primeStream + "]\n")
+  }
+}
---
+++ lectures/src/main/week7/streams/streams.sc
@@ -0,0 +1,42 @@
+object streams {
+  def streamRange(lo: Int, hi: Int): Stream[Int] =
+    if (lo >= hi) Stream.empty
+    else Stream.cons(lo, streamRange(lo + 1, hi))
+
+  def listRange(lo: Int, hi: Int): List[Int] =
+    if (lo >= hi) Nil
+    else lo :: listRange(lo + 1, hi)
+
+  Stream.empty
+  Stream.cons(1, Stream.cons(2, Stream.empty))
+  Stream(1, 2)
+  List()
+  1 :: 2 :: Nil
+  List(1, 2)
+  listRange(1, 1)
+  listRange(1, 5)
+  streamRange(1, 1)
+  streamRange(1, 5)
+  streamRange(1, 5).tail
+  val x = streamRange(1, 10)
+  val evens = (x filter (_ % 2 == 0))
+  evens.tail
+  evens
+  evens.tail.tail
+  evens
+  evens.toList
+  evens
+  evens.tail
+  evens
+  evens.tail.tail.tail
+  evens.tail.tail.tail.tail
+  val z = streamRange(1, 10)
+  z take 3
+  z take 4
+  (z take 3).toList
+  z
+  z take 4
+  z
+  (z take 4).toList
+  z
+}
